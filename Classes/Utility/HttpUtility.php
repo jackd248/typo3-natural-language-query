fe8f512cfa1ceb8d53743652f29ec6cbbfc82920
Konrad Michalik <konrad.michalik@xima.de>
2025-02-17 14:24:18 +0100

    fix: get typo3 request in cli context

--- Classes/Utility/HttpUtility.php
+++ Classes/Utility/HttpUtility.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Kmi\Typo3NaturalLanguageQuery\Utility;
 
 use Psr\Http\Message;
-use TYPO3\CMS\Core;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Http\Uri;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class HttpUtility
 {
@@ -17,6 +24,25 @@ final class HttpUtility
             return $serverRequest;
         }
 
-        return Core\Http\ServerRequestFactory::fromGlobals();
+        return self::fakeTypo3Request();
+    }
+
+    public static function fakeTypo3Request(?Site $site = null, ?SiteLanguage $siteLanguage = null): ServerRequestInterface
+    {
+        if (!$site instanceof Site || !$siteLanguage instanceof SiteLanguage) {
+            $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+            $array = $siteFinder->getAllSites();
+            $site = reset($array);
+            $siteLanguage = $site->getLanguageById(0);
+        }
+
+        $request = new ServerRequest(new Uri((string)$siteLanguage->getBase()));
+        $request = $request->withAttribute('site', $site);
+        $request = $request->withAttribute('language', $siteLanguage);
+        $request = $request->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_FE);
+        $request = $request->withQueryParams(['id' => $site->getRootPageId()]);
+
+        $GLOBALS['TYPO3_REQUEST'] = $request;
+        return $request;
     }
 }
